7299bda402c0b3fa2bc560c843d575fd4f6f060e
Gleb Shkut <gleb.shkut@gmail.com>
2022-02-22 16:02:46 -0800

    added nav menu with days

---
+++ src/components/DayList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList (props) {
+  const {days} = props;
+  const formatedDays = days.map((day) => <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.day} setDay={props.setDay} />)
+  return (
+    <ul>
+      {formatedDays}
+    </ul>
+  );
+}
