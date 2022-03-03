e34457dfc8da072f5c5f0d42622a141122eff6d6
Gleb Shkut <gleb.shkut@gmail.com>
2022-03-03 12:16:03 -0800

    deleted unused content

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,4 @@
-import React from "react";
 import axios from "axios";
-import { getAppointmentsForDay, getDayId, getInterview, getInterviewersForDay } from "helpers/selectors";
 const { useState, useEffect } = require("react");
 
 
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,4 +1,3 @@
-import React from "react";
 const { useState } = require("react");
 
 export default function useVisualMode(initial) {
